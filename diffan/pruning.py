9139636b572e4e540a3fab748a8fab58781d0968
Anilton Cardoso Junior <acardosoj@gmail.com>
2022-12-05 14:26:51 -0300

    updating

--- diffan/pruning.py
+++ diffan/pruning.py
@@ -8,34 +8,34 @@ from diffan.utils import np_to_csv
 import tempfile
 
 def cam_pruning(A, X, cutoff):
-    #save_path = "."
-    with tempfile.TemporaryDirectory() as save_path:
-        if not os.path.exists(save_path):
-           os.makedirs(save_path)   
+    save_path = "."
+    #with tempfile.TemporaryDirectory() as save_path:
+    if not os.path.exists(save_path):
+       os.makedirs(save_path)   
+
+    pruning_path = Path(__file__).parent / "pruning_R_files/cam_pruning.R"
+
+    data_np = X #np.array(X.detach().cpu().numpy())
+    data_csv_path = np_to_csv(data_np, save_path)
+    dag_csv_path = np_to_csv(A, save_path)
+
+    arguments = dict()
+    arguments['{PATH_DATA}'] = data_csv_path
+    arguments['{PATH_DAG}'] = dag_csv_path
+    arguments['{PATH_RESULTS}'] = os.path.join(save_path, "results.csv")
+    arguments['{ADJFULL_RESULTS}'] = os.path.join(save_path, "adjfull.csv")
+    arguments['{CUTOFF}'] = str(cutoff)
+    arguments['{VERBOSE}'] = "FALSE" # TRUE, FALSE
+
+
+    def retrieve_result():
+        A = pd.read_csv(arguments['{PATH_RESULTS}']).values
+        os.remove(arguments['{PATH_RESULTS}'])
+        os.remove(arguments['{PATH_DATA}'])
+        os.remove(arguments['{PATH_DAG}'])
+        return A
     
-        pruning_path = Path(__file__).parent / "pruning_R_files/cam_pruning.R"
-
-        data_np = X #np.array(X.detach().cpu().numpy())
-        data_csv_path = np_to_csv(data_np, save_path)
-        dag_csv_path = np_to_csv(A, save_path)
-
-        arguments = dict()
-        arguments['{PATH_DATA}'] = data_csv_path
-        arguments['{PATH_DAG}'] = dag_csv_path
-        arguments['{PATH_RESULTS}'] = os.path.join(save_path, "results.csv")
-        arguments['{ADJFULL_RESULTS}'] = os.path.join(save_path, "adjfull.csv")
-        arguments['{CUTOFF}'] = str(cutoff)
-        arguments['{VERBOSE}'] = "FALSE" # TRUE, FALSE
-
-
-        def retrieve_result():
-            A = pd.read_csv(arguments['{PATH_RESULTS}']).values
-            os.remove(arguments['{PATH_RESULTS}'])
-            os.remove(arguments['{PATH_DATA}'])
-            os.remove(arguments['{PATH_DAG}'])
-            return A
-        
-        dag = launch_R_script(str(pruning_path), arguments, output_function=retrieve_result)
+    dag = launch_R_script(str(pruning_path), arguments, output_function=retrieve_result)
     return dag
 
 
